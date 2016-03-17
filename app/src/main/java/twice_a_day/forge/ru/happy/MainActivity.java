faf7cef7810f62adb6e7509a64200a3709b64410
Павел <dvajdi@bk.ru>
2016-03-17 21:02:30 +0600

    1

---
+++ app/src/main/java/twice_a_day/forge/ru/happy/MainActivity.java
@@ -0,0 +1,13 @@
+package twice_a_day.forge.ru.happy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
